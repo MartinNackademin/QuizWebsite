a57925cbbb880d6956e5f8d7e775e4ce479c19f1
Martin Mohns <mamoh55@gmail.com>
2025-01-21 03:16:16 +0100

    Installed tailwind,daisyUI,React-Router, and imported/configed all installed libs

--- my-quiz-website/src/App.jsx
+++ my-quiz-website/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from './Components/Header/Header'
+import Footer from './Components/Footer/Footer.jsx'
+import { createHashRouter, RouterProvider, Outlet} from 'react-router-dom';
+import { Home } from './Components/Home/Home';
+import { Quiz } from './Components/Pages/Quiz';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function Layout() {
+    return (
+        <>
+            <Header/>
+            <Outlet/>
+            <Footer/>
+        </>
+    );
 }
 
-export default App
+const router = createHashRouter([
+    {
+        element: <Layout/>,
+        errorElement: <div>Error 404 - Oh my god O_O </div>,
+        children: [
+            {
+                path: "/",
+                element: <Home/>
+            },
+            {
+                path: "/Quiz",
+                element: <Quiz/>
+            },
+        ]
+    }
+]);
+
+function App() {
+    return <RouterProvider router={router} />;
+}
+
+export default App;
