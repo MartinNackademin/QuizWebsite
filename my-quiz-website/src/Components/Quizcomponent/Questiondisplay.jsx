0db698147fd47827faf175aaa2fb683cd8f0b52c
Martin Mohns <mamoh55@gmail.com>
2025-01-24 03:04:38 +0100

    added quiz database import and next question cycle

--- my-quiz-website/src/Components/Quizcomponent/Questiondisplay.jsx
+++ my-quiz-website/src/Components/Quizcomponent/Questiondisplay.jsx
@@ -1,14 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import Cookies from 'js-cookie';
-import QuestionDatabase from './Questiondatabase.json';
+import React from 'react';
 
-function Questiondisplay() {
-
-
+function Questiondisplay({ question, options,handleAnswerClick }) {
+    
 
     
     
-    return null; 
-};
+    return (
+        <>
 
+        <div className="p-4">
+            <h1 className='text-xl font-bold text-center'>{question}</h1>
+        </div>
+
+
+        {options.map((option, uniqueid) => (
+                    <button
+                        key={uniqueid}
+                        className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700"
+                        onClick={() => handleAnswerClick(option)}
+                    >
+                        {option}
+                    </button>
+                ))}
+   
+    </>
+);
+}
+     
 export default Questiondisplay;
