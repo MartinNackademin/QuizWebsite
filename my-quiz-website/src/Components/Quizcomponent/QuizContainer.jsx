e2fcc7767894f72cbc73da32a8d2273da59d4a7b
Martin Mohns <mamoh55@gmail.com>
2025-01-28 15:07:41 +0100

    syntax error

--- my-quiz-website/src/Components/Quizcomponent/QuizContainer.jsx
+++ my-quiz-website/src/Components/Quizcomponent/QuizContainer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Quizmanager from './QuizManger.jsx';
+import QuizManager from './QuizManager.jsx';
 
 function QuizContainer() {
     
@@ -8,7 +8,7 @@ function QuizContainer() {
     <>        
 <div className="flex flex-col justify-center items-center bg-base-300 p-8 rounded-lg shadow-2xl mt-4 text-base-content border-4 border-base-content h-full w-full 
                 max-w-screen-md md:max-w-1400 mx-auto">
-    <Quizmanager/>
+    <QuizManager/>
 </div>
     </>
     );
